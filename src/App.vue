3a41ee071fbf2ab4b43ef71f640502197ca6939c
FernandoSilvera <fernando.silvera02@gmail.com>
2022-06-21 23:20:39 +0200

    Excercise completed.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <TheHeader />
+    <TheFooter />
     <BannerSignUp />
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue'
+import TheFooter from './components/TheFooter.vue'
 import BannerSignUp from './components/BannerSignUp.vue'
 
 export default {
   name: 'App',
   components: {
     TheHeader,
+    TheFooter,
     BannerSignUp
   }
 }
