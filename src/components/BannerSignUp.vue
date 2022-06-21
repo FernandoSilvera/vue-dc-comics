613fcd3952f7ecaeedd1997d99ab9c11c0476b07
FernandoSilvera <fernando.silvera02@gmail.com>
2022-06-21 22:33:11 +0200

    Fix SignUp banner.

--- src/components/BannerSignUp.vue
+++ src/components/BannerSignUp.vue
@@ -3,15 +3,15 @@
     <div class="container">
       <div class="row justify-content-between align-items-center">
         <div class="col d-flex justify-content-start">
-          <button class="text-uppercase my-btn">Sign-Up Now!</button>
+          <button class="text-uppercase">Sign-Up Now!</button>
         </div>
 
         <div class="col row">
-          <div class="col d-flex align-items-center justify-content-end flex-grow-1 text-end">
-            <h3 class="text-uppercase m-0">Follow Us</h3>
+          <div class="col d-flex justify-content-end align-items-center">
+            <h3 class="text-uppercase">Follow Us</h3>
           </div>
 
-          <div class="my-col flex-grow-0" v-for="(image, i) in images"  :key="i">
+          <div class="col flex-grow-0" v-for="(image, i) in images"  :key="i">
             <img :src="`img/footer-${image}.png`" alt="">
           </div>
         </div>
@@ -33,23 +33,18 @@ export default {
 
 <style lang="scss" scoped>
 section {
-    background-color: rgb(63, 63, 63);
+  background-color: rgb(63, 63, 63);
 }
 
-.my-btn {
+button {
   color: white;
   background-color: rgb(63, 63, 63);
   padding: .8rem 1rem;
   border: 2px solid rgb(60, 92, 218);
 }
 
-.my-col {
-    flex-basis: 20px;
-    display: flex;
-    align-items: center;
-}
-
 h3 {
-    color: rgb(60, 92, 218);
+  color: rgb(60, 92, 218);
+  margin: 0;
 }
 </style>
